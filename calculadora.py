8322a9e9e354ec72b312fef9fedb609c4b02ea9e
Kauã Aksenow <kaua.mota2@etec.sp.gov.br>
2024-10-03 19:53:17 -0300

    calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,41 +1,41 @@
-#4 funções, mult., div., som., sub. Primeiro pedir qual operação o cliente deseja fazer
-def divisao():
-    try:
-        div= lista[0] / lista[1]
-        return div
-    except:
-        return "ERRO: divisão por zero"
-        
-def multiplicacao():
-    mult = lista[0] * lista[1]
-    return mult
-    
-def soma():
-    som = lista[0] + lista[1]
-    return som
-    
-def subtracao():
-    sub = lista[0] - lista[1]
-    return sub
-
-operacao = int(input("escolha: 1-divisão; 2-multiplicação; 3-soma; 4-subtração "))
+# 4 funções, mult., div., som., sub. Primeiro pedir qual operação o cliente deseja fazer
 lista = []
+operacao = int(input("escolha: 1-divisão; 2-multiplicação; 3-soma; 4-subtração "))
 def calculadora(): 
+    
+    def divisao():
+        try:
+            resultadodadivisao= lista[0] / lista[1]
+            return resultadodadivisao 
+        except:
+            return "ERRO: divisão por zero"
+            
+    def multiplicacao():
+        resultadodamultiplicacao = lista[0] * lista[1]
+        return resultadodamultiplicacao 
+        
+    def soma():
+        resultadodasoma = lista[0] + lista[1]
+        return resultadodasoma
+        
+    def subtracao():
+        resultadodasubtracao = lista[0] - lista[1]
+        return resultadodasubtracao 
 
-    for i in range(1):
+
+    for i in range(2):
         num = float(input("digite o número:"))
         lista.append(num)
     if operacao == 1:
-        print(f"O resultado da sua Divisão é {divisao()}")
+        return (divisao())
     elif operacao == 2:
-        print(f"O resultado da Multiplicação é {multiplicacao()}")
+        return(multiplicacao())
     elif operacao == 3:
-        print(f"O resultado da soma é {soma()}")
+        return(soma())
     elif operacao == 4:
-        print(f"O resultado da subtração é {subtracao()}")
-    return calculadora()
-print(calculadora())
-
+        return(subtracao())
+        
+print (f'O Resultado é: {(calculadora())}')
 
 
     
